93c32bd4257036147d676476e96c65bb6df97e22
KingOfLuckyGuessing <stephen2015usa@gmail.com>
2022-11-01 10:42:50 -0500

    main changes

--- main.cpp
+++ main.cpp
@@ -1,22 +1,24 @@
 #include <iostream>
+#include <stdlib.h>
 #include "Node.h"
 #include "BST.h"
 using namespace std;
 
 int main() {
-    BST bst(5);
+    BST bst(-1);
+    Node** n = new Node*[10];
+    //-1,-4,10,-5,5,100,2,7,1,9
+    int nums[] = {10,5,100,-4,2,1,7,9};
 
-    Node* n1 = new Node(6);
-    bst.add(n1);
+    for(int i = 0; i < 10; i++){
+        n[i] = new Node(nums[i]);
+    }
 
-    Node* n2 = new Node(7);
-    bst.add(n2);
+    for(int i = 0; i < 10; i++){
+        bst.add(n[i]);
+    }
 
-    Node* n4 = new Node(3);
-    bst.add(n4);
 
-    Node* n5 = new Node(9);
-    bst.add(n5);
 
     cout << bst.getHeight() << std::endl;
 
